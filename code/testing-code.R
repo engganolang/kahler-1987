2034248cde5c8da09f902d9773de9eba9bcda792
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2023-06-20 06:23:48 +0800

    Add exploration codes to count variant forms per stem, extract certain cross-references, and etymological source language.

--- code/testing-code.R
+++ code/testing-code.R
@@ -7,6 +7,11 @@ kstem <- read_xlsx("data-raw/kahler1.xlsx", range = "A29:X2117") |>
   mutate(kms_Alphabet = str_to_lower(kms_Alphabet)) |> 
   arrange(as.numeric(kms_page), kms_Alphabet, as.numeric(kms_entry_no))
 
+lang_abbr <- read_xlsx("data-raw/kahler1.xlsx", range = "A5:B27") |> 
+  mutate(sw_name = replace(sw_name, sw_name %in% c("PAN", "UAN (Proto-Austronesian)"), "PAN (Proto-Austronesian)"),
+         sw_name = replace(sw_name, sw_name == "MKB", "MKB (Minangkabau [Austronesian, Sumatra])"),
+         sw_name = replace(sw_name, sw_name == "SMtw", "SMtw (Sudmentawai [South Mentawai])"))
+
 # read the example ====
 ## IMPORTANT: Re-check the `range` when the transcription is done!
 kex <- read_xlsx("data-raw/kahler1.xlsx", range = "A2119:T5500")
@@ -90,14 +95,42 @@ kstem |>
   filter(if_any(.cols = everything(), ~str_detect(., unique(all_chars_stem_tb$char)[115])))
 
 
+### test to tokenise the separator of the variant providing whitespace in either side
+kstem <- kstem |> 
+  mutate(stem_formVarian = str_replace_all(stem_formVarian, "(?<!\\s)([;,])", " \\1"))
+
+### test to count variant of the stems =====
+kstem |> 
+  mutate(variant_forms = str_count(stem_formVarian, "\\b[^,;]+\\b")) |> 
+  select(stem_form, stem_formVarian, variant_forms) |> 
+  filter(!is.na(variant_forms)) |> 
+  arrange(desc(variant_forms)) |> 
+  print(n=20)
+
+### test to measure mean of variant forms of the stem =====
+kstem |> 
+  mutate(variant_forms = str_count(stem_formVarian, "\\b[^,;]+\\b")) |> 
+  select(stem_form, stem_formVarian, variant_forms) |> 
+  filter(!is.na(variant_forms)) |> 
+  arrange(desc(variant_forms)) |> summarise(mean_var = mean(variant_forms))
+
+### test count the source language marked as etymology =====
+kstem |> 
+  select(matches("etym.*donor")) |> 
+  filter(!is.na(stem_etymological_language_donor)) |> 
+  left_join(lang_abbr |> 
+              rename(stem_etymological_language_donor = sw_id)) |> 
+  count(sw_name, sort = TRUE)
 
 
+### test finding any appearance of Helfrich's 1916 or Helfrich's 1888 =====
+kstem |> 
+  filter(if_any(everything(), ~str_detect(., "\\b[Hh]16\\b"))) |> 
+  select(stem_crossref)
 
-
-
-
-
-
+kstem |> 
+  filter(if_any(everything(), ~str_detect(., "\\b[Hh]8"))) |> 
+  select(stem_crossref)
 
 
 
@@ -109,17 +142,17 @@ kstem |>
 
 
 # get the characters - EXAMPLES ======
-all_chars_stem <- kex |> 
-  select(!matches("create|date|update|used"), -kms_Alphabet, -kms_page, -kms_entry_no) |> 
-  pivot_longer(-stem_id, names_to = "cols_name") |> 
-  pull(value) |> 
-  str_split("\\W") |> 
-  map(function(x) x[nzchar(x)]) |> 
-  unlist() |> 
-  str_split("") |> 
-  unlist() |> 
-  sort()
-all_chars_stem_tb <- tibble(char = all_chars_stem) |> 
-  count(char) |> 
-  arrange(char, desc(n))
-all_chars_stem_tb$char |> unique()
+# all_chars_stem <- kex |> 
+#   select(!matches("create|date|update|used"), -kms_Alphabet, -kms_page, -kms_entry_no) |> 
+#   pivot_longer(-stem_id, names_to = "cols_name") |> 
+#   pull(value) |> 
+#   str_split("\\W") |> 
+#   map(function(x) x[nzchar(x)]) |> 
+#   unlist() |> 
+#   str_split("") |> 
+#   unlist() |> 
+#   sort()
+# all_chars_stem_tb <- tibble(char = all_chars_stem) |> 
+#   count(char) |> 
+#   arrange(char, desc(n))
+# all_chars_stem_tb$char |> unique()
