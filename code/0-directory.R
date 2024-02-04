2e30de9dd295b30c53a890242a37f7eb2e1e5c4e
Enggano <engganolang@gmail.com>
2024-02-04 11:34:12 +0800

    Pending tracking the directory code file until all pre-processing is done.

--- code/0-directory.R
+++
@@ -1,5 +0,0 @@
-# Enggano main folder URL/ID
-enggano_folder <- "https://drive.google.com/drive/folders/1MO3Q9KZIODxlfPRyjLTtDUZuVkecFBp6"
-
-# Kahler dictionary translation folder id
-kahler_dict_folder <- "https://drive.google.com/drive/folders/1KY-XunZ0mzkXxCJbbrOFDH1vgmCBTpSV"
