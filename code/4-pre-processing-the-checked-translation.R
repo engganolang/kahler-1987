13b3dc3c8d1c9991dab474dc1c0b23d98cc17dca
Gede Rajeg <primahadiwijaya@gmail.com>
2024-11-17 15:56:14 +0800

    Add header marker in the code file and note to fix any error in the GSheet file.

--- code/4-pre-processing-the-checked-translation.R
+++ code/4-pre-processing-the-checked-translation.R
@@ -1,4 +1,5 @@
 # NOTE: This code file processes the checked translation data for the stems and example forms. It involves retrieving the checked English translation from the Google Spreadsheet, save it into .rds data, and do further editing.
+# IMPORTANT: Edits any typos in German translation in the GERMAN_CORRECTED column in the GSheet!
 
 # library(googledrive)
 # library(googlesheets4)
@@ -11,7 +12,7 @@ library(tidyverse)
 # kahler_dict_files <- drive_ls(path = as_id(kahler_dict_folder))
 # kahler_dict_files
 
-# get the stem translation from the Google Spreadsheet that has been checked
+# get the stem translation from the Google Spreadsheet that has been checked ==========
 # stems_translation_checked <- drive_get("1_stem_german_translation-to-check") |>
 #   read_sheet() |>
 #   write_rds("data-raw/1_stem_german_translation-to-check.rds")
@@ -63,7 +64,7 @@ stems_translation_checked1 <- stems_translation_checked |> # combine the correct
                                  category == "stem_crossref",
                                "DEC 345 kadodo 'Artocarpus Gomeziana Wall.', DEC 355 kadodotok 'Artocarpus rigilda bl.', Heyne 64/1946 kadodohok 'Artocarpus rigida'"))
 
-# get the concept column from the example translation GSheet
+# get the concept column from the example translation GSheet ==========
 # ex_concept <- drive_get("2_example_german_translation") |>
 #   read_sheet() |>
 #   filter(!is.na(Concept), Concept != "DUPLICATE") |>
@@ -74,7 +75,8 @@ stems_translation_checked1 <- stems_translation_checked |> # combine the correct
 # ex_concept |> write_rds("data-raw/ex_concept.rds")
 ex_concept <- read_rds("data-raw/ex_concept.rds")
 
-# get the example translation Google Spreadsheet that has been checked
+# get the example translation Google Spreadsheet that has been checked =========
+## BATCH 1 =====
 # ex_translation_checked1 <- drive_get("2_example_german_translation-to-check-BATCH1") |>
 #   read_sheet() |>
 #   mutate(batch = 1)
@@ -83,6 +85,7 @@ ex_concept <- read_rds("data-raw/ex_concept.rds")
 # load the .rds file, which comes from the Google Spreadsheet containing the English translation for EXAMPLE FORM (BATCH 1) that has been checked
 ex_translation_checked1 <- read_rds("data-raw/2_example_german_translation-to-check-BATCH1.rds")
 
+## BATCH 2 =====
 # ex_translation_checked2 <- drive_get("2_example_german_translation-to-check-BATCH2") |>
 #   read_sheet() |>
 #   mutate(batch = 2)
